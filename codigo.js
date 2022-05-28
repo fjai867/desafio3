f14ef9e79d6473705daa0d68c2aff7212caa68e3
paco alonso <fjai867u@gmail.com>
2022-05-29 00:47:32 +0200

    desfio3 1.1

---
+++ codigo.js
@@ -0,0 +1,65 @@
+
+document.getElementById("dia").addEventListener('click',traerDatos)
+document.getElementById("wk").addEventListener('click',traerDatos1)
+document.getElementById("mt").addEventListener('click',traerDatos2)
+
+
+const xhttp=new XMLHttpRequest();
+let datos2
+xhttp.open('GET','data.json',true);
+xhttp.send();
+xhttp.onreadystatechange= function(){
+
+    if(this.readyState== 4 && this.status== 200){
+           
+        let datos=JSON.parse(this.responseText);
+        console.log(datos)
+        datos2=datos
+        }
+}
+
+const ides=["work","play","study","exercice","social","self"]
+
+function traerDatos(){ 
+  
+let numId=0
+for (let item of datos2) {
+    
+    document.getElementById(ides[numId]).innerHTML=
+        `
+        <h3>${item.timeframes.daily.current} hrs </h3>
+        <h5>last week ${item.timeframes.daily.previous} hrs </h5>
+        ` 
+    numId++; 
+        } 
+}
+
+function traerDatos1(){ 
+  
+    let numId=0
+    for (let item of datos2) {
+           
+        document.getElementById(ides[numId]).innerHTML=
+            `
+            <h3>${item.timeframes.weekly.current} hrs </h3>
+            <h5>last week ${item.timeframes.weekly.previous} hrs </h5>
+            ` 
+        numId++; 
+            } 
+    }
+
+    function traerDatos2(){ 
+  
+        let numId=0
+        for (let item of datos2) {
+               
+            document.getElementById(ides[numId]).innerHTML=
+                `
+                <h3>${item.timeframes.monthly.current} hrs </h3>
+                <h5>last week ${item.timeframes.monthly.previous} hrs </h5>
+                ` 
+            numId++; 
+                } 
+        }   
+
+    
